d9e0d84d9f1f3a20acf1a2eda81409dbb090b950
Francis054 <francisdey054@gmail.com>
2026-07-19 08:05:32 +0000

    Stage 1: root and health endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express, { type Request, type Response, type Application } from "express";
+import express, {
+  type Request,
+  type Response,
+  type Application,
+} from "express";
 
 const app: Application = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response): void => {
-  res.status(200).send('Hello World!');
+  res.status(200).json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: "[/tasks]",
+  });
 });
+
+app.get("/health", (req:Request, res:Response): void => {
+  res.status(200).json({
+    status: "ok"
+  })
+})
 
 const PORT: number = 3000;
 
