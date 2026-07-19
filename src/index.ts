6efa0262c6061722b4e96f04f122707ec6130f44
Francis054 <francisdey054@gmail.com>
2026-07-19 08:54:53 +0000

    Stage 2: read endpoints with 404

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,30 @@ const app: Application = express();
 
 app.use(express.json());
 
+type Task = {
+  id: number;
+  title: string;
+  done: boolean;
+};
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    title: "Learn Express",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "Learn TypeScript",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "Build a Todo API",
+    done: false,
+  },
+];
+
 app.get("/", (req: Request, res: Response): void => {
   res.status(200).json({
     name: "Task API",
@@ -16,11 +40,31 @@ app.get("/", (req: Request, res: Response): void => {
   });
 });
 
-app.get("/health", (req:Request, res:Response): void => {
+app.get("/health", (req: Request, res: Response): void => {
   res.status(200).json({
-    status: "ok"
-  })
-})
+    status: "ok",
+  });
+});
+
+app.get("/tasks", (req: Request, res: Response): void => {
+  res.status(200).json({
+    tasks,
+  });
+});
+
+app.get("/tasks/:id", (req: Request, res: Response) => {
+  const id: number = Number(req.params.id);
+
+  const task = tasks.find((task) => task.id === id);
+
+  if (!task) {
+    res.status(404).json({
+      error: `Task ${id} not found`,
+    });
+    return;
+  }
+  res.status(200).json(task);
+});
 
 const PORT: number = 3000;
 
